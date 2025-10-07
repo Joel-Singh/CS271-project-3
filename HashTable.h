b59f78cf9da3afaf2960bb05efc46cd0a147affa
Nanakz56 <164203862+Nanakz56@users.noreply.github.com>
2025-10-07 13:25:20 -0400

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -1,18 +1,37 @@
+//========================================================
+// CS 271
+// Fall 2025
+// HashTable.h
+// This file contains the HashTable class declaration.  
+//========================================================
+
+#include <iostream>
+#include <string>
+#include "Element.h"
+
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-#include <string>
-using namespace std;
+template<class T>
+class HashTable{
+    public:
+                            HashTable           ( void );
+                            HashTable           ( int size );
+                            ~HashTable          ( void );
+        void                insert              (T data, int k);
+        void                remove              (T data, int k);
+        bool                member              (T data, int k);
+        std::string         to_string           ( void ); 
+    private:
+        int m; //size of hash table
 
-template <typename T> class HashTable {
-public:
-  // Not sure what the constructor parameter is actually supposed to be
-  HashTable<T>(int size);
-  void insert(T data, int key);
-  void remove(T data, int key);
-  bool member(T data, int key);
-  string to_string();
+        T *arr; //hash table built on an array
+
+        int             default_HashFunc        ( int key );
+        bool            verifyIndex             (int index);
+
 };
 
 #include "HashTable.cpp"
+
 #endif
