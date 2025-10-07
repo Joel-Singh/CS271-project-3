6ebf4c41e7f85db5c261d5826ec5feb4f54f3307
Joel Singh <joelsingh788@gmail.com>
2025-10-07 13:25:31 -0400

    Add nana's Element.h and Element.cpp

--- Element.cpp
+++ Element.cpp
@@ -1,9 +1,132 @@
-#include "./Element.h"
+/*
+====================================================
+Project 3: Smushed Universes (Hash Table Implementaion)
+Written by: Nana K. Amo-Mensah and Joel Singh
+Date Updated: 10.07.2025
+Description: Implementation of a templated
+Element Class
+Program functionality: Passes all test cases
+=====================================================
+*/
 
-template <typename T> Element<T>::Element() {}
+#include <iostream>
+#include <sstream>
+#include "Element.h"
 
-template <typename T> Element<T>::Element(T data, int key) : Element() {}
 
-template <typename T> int Element<T>::get_key() {}
+/*
+================================================
+Default Constructor
+Creates an empty element. 
+    Data value is set equal to zero
+    Key becomes -1.
+Call: Element<T> e;
+Return: none. element object is constructed 
+================================================
+*/
+template<typename T>
+    Element<T>:: Element ( void ){
+        key = -1;
+}
 
-template <typename T> T Element<T>::get_data() {}
+/*
+=====================================================
+Constructor
+Creates an element using the passed in data values.
+    The key is always going to be a numeric value. 
+    The actual data can be of any type.
+Pre: Element<T> e(T data, int key);
+Post: Element is constructed.
+======================================================
+*/
+template<typename T>
+    Element<T>:: Element ( T d, int k ){
+        key = k;
+        data = d; 
+}
+
+/*
+================================================
+Destructor
+Destroys object when it goes out of scope or at
+the end of the program.
+Call: done automatically
+Return: deallocates memory
+================================================
+*/
+template<typename T>
+    Element<T>:: ~Element ( void ){
+}
+
+/*
+================================================
+operator=
+Copies the values of the passed-in object 
+and returns a reference to the modified element/
+Call: elemObjt1 = elemObjct2
+Return: type(*this) --> Element<T>
+================================================
+*/
+template<typename T>
+    Element<T> Element<T>:: operator= ( const Element<T> &e){
+        data = e.data;
+        key = e.key;
+        return (*this);
+}
+
+
+/*
+================================================
+get_key()
+Returns the key of the element object.
+Call: elemObject.get_key().
+Return: type(key) --> it
+================================================
+*/
+template<typename T>
+    int Element<T>:: get_key ( void ) const {
+        return key;
+}
+
+
+/*
+================================================
+get_data()
+Returns the data of the element object.
+Call: elemObject.get_data().
+Return: type(key) --> any
+================================================
+*/
+template<typename T>
+    T Element<T>:: get_data ( void ) const {
+        return data;
+}
+
+/*
+================================================
+set_data()
+Modifies the data of an element object.
+Call: elemObject.set_data( T d ).
+Return: none.
+================================================
+*/
+template<typename T>
+    void Element<T>:: set_data ( T d ){
+        data = d;
+}
+
+/*
+================================================
+to_string()
+Returns a string representation of the element 
+object
+Call: elemObject.to_string().
+Return: type(retVal) --> String
+================================================
+*/
+template<typename T>
+    std::string Element<T>:: to_string (  void ) const {
+        std::stringstream retVal;
+        retVal << "("<< get_data()<< ","<<get_key()<<")"; 
+        return retVal.str();
+}
--- Element.h
+++ Element.h
@@ -1,13 +1,33 @@
+//========================================================
+// CS 271
+// Fall 2025
+// Element.h
+// This file contains the Element class declaration.  
+//========================================================
+
+#include <iostream>
+#include <string>
+
 #ifndef ELEMENT_H
 #define ELEMENT_H
 
-template <typename T> class Element {
-public:
-  Element();
-  Element(T data, int key);
-  int get_key();
-  T get_data();
+template<class T>
+class Element{
+    public:
+                                Element         ( void );
+                                Element         (T d, int k);
+                                ~Element        ( void );
+        Element<T>              operator=       ( const Element<T> &e);
+        int                     get_key         ( void ) const;
+        T                       get_data        ( void ) const;
+        void                    set_data         ( T d);
+        std::string             to_string       ( void ) const; 
+    private:
+        int key;
+        T data;
 };
 
+
 #include "Element.cpp"
+
 #endif
